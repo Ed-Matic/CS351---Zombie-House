946abfde52dcab62d7a98a404457bdf2901b99e5
MBarnhart <MBarnhart@unm.edu>
2016-03-08 03:06:04 -0700

    Additional Constructor values
    The Zombie class now natively holds its speed and smell distance.  In the future, it can also hold its A* algorithm as long as we pass it in the floorPlan of the house.
    
    Added an extra XForm so that movement more closely reflects the movement of the camera in the main class.

--- Zombie.java
+++ Zombie.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Random;
+
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -9,28 +11,60 @@ public class Zombie extends MovingBody
 {
   private final boolean RANDOM_WALK;
   private boolean playerDetected;
-  protected Xform zombieXform = new Xform();
+  public Xform zombieXYrotate = new Xform();
+  public Xform zombieXYtranslate = new Xform();
+  Random zRandom = new Random();
   
-  public Zombie(boolean zombieType, double xPos, double yPos, double radius)
+  private final double speed;
+  private final double smellDistance;
+  
+  public Zombie(boolean zombieType, double xPos, double yPos, double radius, double zSpeed, double smellDist)
   {
     super(xPos, yPos, radius);
     RANDOM_WALK = zombieType;
     playerDetected = false;
-    zombieXform.setTranslateX(xPos);
-    zombieXform.setTranslateZ(yPos);
-    zombieXform.setTranslateY(-50);
+    speed = zSpeed;
+    smellDistance = smellDist;
+    
+    
+    zombieXYtranslate.setTranslateX(xPos);
+    zombieXYtranslate.setTranslateZ(yPos);
+    zombieXYtranslate.setTranslateY(-50);
+    zombieXYtranslate.getChildren().add(zombieXYrotate);
+    zombieXYrotate.getChildren().add(drawZombie());
   }
   
   @Override
   public void move()
   {
     if (playerDetected); //A* Pathfinding
-    else if (RANDOM_WALK); //Random walk movement
+    else if (RANDOM_WALK) {
+      randomWalk();//Random walk movement
+    }
     else; //Line Walk movement
   }
   
-  //METHODS TO BE ADDED?
-  //drawZombie() 
+  public void randomWalk()
+  {
+    double yAngle = zombieXYrotate.ry.getAngle() * Math.PI / 180;
+    double xAnglePercentage = Math.sin(yAngle) * Math.sin(yAngle);
+    double zAnglePercentage = Math.cos(yAngle) * Math.cos(yAngle);
+    
+    zombieXYtranslate.t.setZ(zombieXYtranslate.t.getZ() + speed*zAnglePercentage);
+    zombieXYtranslate.t.setX(zombieXYtranslate.t.getX() + speed*xAnglePercentage);
+  }
+  
+  public void changeDirection()
+  {
+    float newDirection = zRandom.nextFloat()*360;
+    //DEPENDING ON HOW WE IMPLEMENT COLLISION, WE'LL DO MORE DETECTION HERE
+    zombieXYrotate.setRotateY(newDirection);
+  }
+  
+  public void randomMove()
+  {
+    
+  }
   
   public Node drawZombie()
   {
@@ -41,6 +75,11 @@ public class Zombie extends MovingBody
     Cylinder zombieCylinder = new Cylinder(getRadius(), 150);
     zombieCylinder.setMaterial(material);
     return zombieCylinder;
+  }
+
+  public Xform getXform()
+  {
+    return zombieXYtranslate;
   }
   
   
